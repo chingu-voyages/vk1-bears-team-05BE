c52e1fb9ba179984148a657a9c87681153e57a5c
aynfrancesco06 <56792014+aynfrancesco06@users.noreply.github.com>
2020-11-27 04:35:12 +0800

    add_branch_shit

---
+++ src/components/Body.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Body = () => {
+    return (
+        <div>
+            <h1> Hello this is the body</h1>
+            <h2> branch_test executed changes  </h2>
+            <h3> new branch change test</h3>
+            <h4> added a new shit in new_branch</h4> 
+        </div>
+    )
+}
+
+export default Body;
