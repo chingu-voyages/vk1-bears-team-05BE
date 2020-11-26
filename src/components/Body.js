f64691c4d64b5cf8a00287845c6a8e9e77f69b69
aynfrancesco06 <56792014+aynfrancesco06@users.noreply.github.com>
2020-11-27 07:50:54 +0800

    added a login feature_test

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,6 +5,7 @@ const Body = () => {
         <div>
             <h1> Hello this is the body</h1>
             <h2> branch_test executed changes  </h2>
+            <h3>This is a login feature_test</h3> 
     )
 }
 
