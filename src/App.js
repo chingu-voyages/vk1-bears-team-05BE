9d32c9a82dd0cfd246b85db3c5d625e83a80afd4
Ricky Galicinao <ricky.seblos@gmail.com>
2020-12-11 00:45:24 +0800

    fixed header, registration validation, body margin

--- src/App.js
+++ src/App.js
@@ -1,42 +1,36 @@
-
-import React, { Component , Fragment, useState } from "react";
-import { BrowserRouter as Router, Route,} from 'react-router-dom';
-import "./App.css";
+import React, { Component, Fragment, useState } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 import SetupProfile from "./components/pages/SetupProfile";
 import SignInPage from "./components/pages/SignIn";
 import ForgotPW from "./components/pages/ForgotPW";
-import Home from "./components/pages/Home/Home"
-import Register from "./components/pages/Register"
-import EditProfilePage from "./components/pages/EditProfilePage"
+import Home from "./components/pages/Home/Home";
+import Register from "./components/pages/Register";
+import EditProfilePage from "./components/pages/EditProfilePage";
 import Switch from "react-bootstrap/esm/Switch";
-
-
+import "./App.css";
 
 const App = () => {
-
-
   return (
     <Fragment>
       <Router>
         <div>
           <Header />
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route path='/signInPage' component={SignInPage} />
-              <Route path='/register' component={Register} />
-              <Route path='/forgotPW' component={ForgotPW} />
-              <Route path='/setupProfile' component={SetupProfile} />
-              <Route path='/editProfilePage' component={EditProfilePage} />
-            </Switch>
+          <Switch className='switchLayout'>
+            <Route exact path='/' component={Home} />
+            <Route path='/signInPage' component={SignInPage} />
+            <Route path='/register' component={Register} />
+            <Route path='/forgotPW' component={ForgotPW} />
+            <Route path='/setupProfile' component={SetupProfile} />
+            <Route path='/editProfilePage' component={EditProfilePage} />
+          </Switch>
           <Footer />
         </div>
       </Router>
 
-
-    <script
+      <script
         src='https://unpkg.com/react/umd/react.production.min.js'
         crossorigin
       ></script>
@@ -50,16 +44,11 @@ const App = () => {
         src='https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js'
         crossorigin
       ></script>
-
-
     </Fragment>
-
   );
 };
 
 export default App;
-
-
 
 // // manual user
 // const adminUser = {
@@ -91,18 +80,7 @@ export default App;
 //   setUser({ name: "", email: "" });
 // }
 
-
-
-
-
-
-
-
-
-
-
 //<SignInPage SignIn={SignIn} error={error} />
-
 
 // {(user.email !== "") ? (
 //   // <div className="welcome">
@@ -110,12 +88,8 @@ export default App;
 //   //   <button onClick={Logout}>Logout</button>
 //   // </div>
 
-  
 //          //successful sign in
-  
 
 //    ) : (
-  
-// )}  
 
-
+// )}
